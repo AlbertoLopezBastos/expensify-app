f063cdf457dcd205078cba02b92b966010273307
Alberto <elbeto182@hotmail.com>
2020-05-25 18:34:43 -0300

    Setup formatting for amount and createdAt

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -7,4 +7,5 @@ import expenses from '../fixtures/expenses.js';
 test('should render ExpenseListItem correctly', () => {
     const wrapper = shallow(<ExpenseListItem expense={expenses[0].description, expenses[0].amount, expenses[0].createdAt, expenses[0].id} />);
     expect(toJSON(wrapper)).toMatchSnapshot();
-})
+});
+
